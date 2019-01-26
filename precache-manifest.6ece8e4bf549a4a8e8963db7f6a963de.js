f8c6ac2969937025dec746748833edba023ead24
Ruslan Baybekov <oxyii.dev@gmail.com>
2019-01-26 21:58:29 +0300

    Updates

---
+++ precache-manifest.6ece8e4bf549a4a8e8963db7f6a963de.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "40059386b35b4c024b22",
+    "url": "/react-native-web-swiper/static/js/main.40059386.chunk.js"
+  },
+  {
+    "revision": "fe646c640d989a6dd2aa",
+    "url": "/react-native-web-swiper/static/js/1.fe646c64.chunk.js"
+  },
+  {
+    "revision": "590f8c90eb1c85de0dbc",
+    "url": "/react-native-web-swiper/static/js/runtime~main.590f8c90.js"
+  },
+  {
+    "revision": "4b3ffc3be6ee32ed050472e9ffa9a6e3",
+    "url": "/react-native-web-swiper/index.html"
+  }
+];
